640e6573458a2d050e21f24b1f1eaed6e9be60b0
bo yang <1003208172@qq.com>
2021-05-23 16:09:14 +0800

    update admin
    准备增加管理端

---
+++ resources/views/admin/main.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet/less" href="{{asset("css/main.less")}}">
+
+    <title>模板框架</title>
+    <script src="{{asset("js/less.min.js")}}"></script>
+</head>
+<body>
+    <div id="app">
+        <div class="header">
+            <div class="left">
+                <p class="title">boss直拒 管理端</p>
+            </div>
+            <div class="right">111</div>
+        </div>
+        <div class="main">
+            <div class="nav">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('main', function () {
+    return view('admin.main');
+});
